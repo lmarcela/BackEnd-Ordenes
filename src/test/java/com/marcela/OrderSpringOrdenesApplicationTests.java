24c6d029d6d1ca442cd2e3922b607e57c4b3d48a
Marcela Malaver <marcela9409@gmail.com>
2018-01-09 20:37:01 -0500

    Order Tests added. GetOrder, getOrdersCustomerDates, getOrderCustomerLastMonth

--- src/test/java/com/marcela/OrderSpringOrdenesApplicationTests.java
+++ src/test/java/com/marcela/OrderSpringOrdenesApplicationTests.java
@@ -16,7 +16,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 
@@ -40,9 +39,17 @@ public class OrderSpringOrdenesApplicationTests {
 				.perform(get("/orders").contentType(MediaType.APPLICATION_JSON)
 						.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
 				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
-		// Order[] order = new ObjectMapper().readValue(jsonResponse, Order[].class);
-		// Assert.assertNotNull(order);
 		System.out.println("Exito en getOrders con los datos: \n " + JsonPath.parse(jsonResponse).json());
+	}
+	
+	@Test
+	public void getOrder() throws Exception {
+		String jsonResponse = mockMvc
+				.perform(get("/order/1").contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+		Assert.assertFalse(new ObjectMapper().readTree(jsonResponse).get("customerName").asText().isEmpty());
+		System.out.println("Exito en getOrder (1) con los datos: \n " + JsonPath.parse(jsonResponse).json());
 	}
 
 	@Test
@@ -51,10 +58,25 @@ public class OrderSpringOrdenesApplicationTests {
 				.perform(get("/order/customer/" + 1).contentType(MediaType.APPLICATION_JSON)
 						.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
 				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
-		// Order order = new ObjectMapper().readValue(jsonResponse, Order.class);
-		// Assert.assertNotNull(order);
-		// Assert.assertFalse(order.getCustomerName().isEmpty());
 		System.out.println("Exito en getOrdersCustomer con los datos: \n " + JsonPath.parse(jsonResponse).json());
+	}
+
+	@Test
+	public void getOrdersCustomerDates() throws Exception {
+		String jsonResponse = mockMvc
+				.perform(get("/order/customer/1/desde/2017-01-10/hasta/2018-02-05").contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+		System.out.println("Exito en getOrdersCustomerDates con los datos: \n " + JsonPath.parse(jsonResponse).json());
+	}
+
+	@Test
+	public void getOrderCustomerLastMonth() throws Exception {
+		String jsonResponse = mockMvc
+				.perform(get("/order/customer/1/ultimoMes").contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+		System.out.println("Exito en getOrderCustomerLastMonth con los datos: \n " + JsonPath.parse(jsonResponse).json());
 	}
 
 	@Test
@@ -75,16 +97,7 @@ public class OrderSpringOrdenesApplicationTests {
 				.perform(post("/order/").content(raw).contentType(MediaType.APPLICATION_JSON)
 						.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
 				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		;
-		JsonNode jsonNode = objectMapper.readTree(jsonResponse);
-		String customerName = jsonNode.get("customerName").asText();
-
-		Assert.assertFalse(customerName.isEmpty());
-
-		System.out.println("Exito en createOrder con los datos: \n " + JsonPath.parse(jsonResponse).json()
-				+ " -customer: " + customerName);
-
+		Assert.assertFalse(new ObjectMapper().readTree(jsonResponse).get("customerName").asText().isEmpty());
+		System.out.println("Exito en createOrder con los datos: \n " + JsonPath.parse(jsonResponse).json());
 	}
 }
